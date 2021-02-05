a7aa9695076038a7adb64e434818329acd8013a8
Altair Sossai <altairsossai@gmail.com>
2021-02-05 17:10:41 -0300

    Inicio do projeto

---
+++ Zeats.Stream/Extensions/DeserializeExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Zeats.Json;
+
+namespace Zeats.Stream.Extensions
+{
+    public static class DeserializeExtension
+    {
+        public static async Task<T> Deserialize<T>(this System.IO.Stream stream)
+        {
+            return await JsonParser.DeserializeAsync<T>(stream);
+        }
+    }
+}
